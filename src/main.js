1fc3aca81aec263ba62d3bda51c15aa9ea9ebfbf
thethtun <thethtun126@gmail.com>
2025-05-02 22:18:12 +0900

    watch the user is nothing go home

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import App from './App.vue'
 import router from './router'
 import "./assets/global.css"
 import { auth } from './firebase/config'
+
 let app;
 
-
-//Wait for Firebase to check user login state
+            //Wait for Firebase to check user login state
 auth.onAuthStateChanged(()=> {
 
-    // Make sure the app mounts only once   if(!app)
+            // Make sure the app mounts only once   if(!app)
     if(!app) {
        app =createApp(App).use(router).mount('#app') 
     }
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -7,9 +7,26 @@
 </template>
 
 <script>
+
+import getUser from '../composables/getUser'
 import Navbar from '../components/Navbar'
+import { useRouter } from 'vue-router'
+import { watch } from 'vue'
+
+
 export default {
   components: { Navbar },
+  setup () {
+    let {user} = getUser();
+    let router = useRouter();
+            // Every watching the user 
+    watch(user,()=> {
+        if(!user.value) {
+            router.push("/") // user is nothing  go homepage
+
+        }
+    })
+  }
 
 }
 </script>
