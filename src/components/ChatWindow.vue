c8ce62b7f60a3357a12cc687a8db45032de95a69
thethtun <thethtun126@gmail.com>
2025-05-05 22:02:03 +0900

    scrollbar top and height

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="chat-window">
-    <div class="message" v-for="message in messages" :key="message.id">
-        <div class="single">
-            <span class="created-at">{{message.created_at.toDate()}}</span>
+    <div class="message" ref="msgBox">
+        <div class="single"  v-for="message in formattedMessages" :key="message.id">
+            <span class="created-at">{{message.created_at}}</span>
             <span class="name">{{message.name}}</span>
             <span class="message">{{message.message}}</span>
 
@@ -14,35 +14,52 @@
 </template>
 
 <script>
-import { ref } from 'vue'; 
+import { computed, onUpdated, ref } from 'vue'; 
 import {db} from "../firebase/config";
+import {formatDistanceToNow} from "date-fns"
 export default {
     setup() {
         let messages = ref([]);
-         let result = [] //doc is array
+        let msgBox = ref(null);
+        onUpdated(() => {
+            // console.log(msgBox.value.scrollTop)
+            msgBox.value.scrollTop = msgBox.value.scrollHeight
+        })
+                // computed()is return array,{},string,...
+                // formattedMessages is [] now
+        let formattedMessages = computed(()=> {
+            return messages.value.map((msg) => {
+                                    //created _at is changing toDate() here 
+                let formatTime = formatDistanceToNow(msg.created_at.toDate())
+                return {...msg,created_at:formatTime}
+
+            })
+
+        })
 
         db.collection("messages").orderBy("created_at").onSnapshot((snap) => {
+            let result = [] //doc is array
             // docs is array[]
             snap.docs.forEach((doc) => {
-                // console.log(doc);
-                // doc.data() ကိုခေါ်လိုက်တာနဲ့ ဒီ document ထဲမှာ သိမ်းထားတဲ့ field တွေကို JavaScript object အနေနဲ့ ပြန်ပေးပါတယ်။
-                //  doc.data() call document filed javascript object { "name": "mg mg"}  => { name: "mg mg"}
+                        // console.log(doc);
+                        // doc.data() ကိုခေါ်လိုက်တာနဲ့ ဒီ document ထဲမှာ သိမ်းထားတဲ့ field တွေကို JavaScript object အနေနဲ့ ပြန်ပေးပါတယ်။
+                        //  doc.data() call document filed javascript object { "name": "mg mg"}  => { name: "mg mg"}
 
                 let document = {...doc.data(),id: doc.id}
 
-                //problem error of created_at null is solve
-                // if(doc.data().created_at) {
-                //     result.push(document)
-                // }
-                // if created_at is data inside run this code result.push(document)
-                // 1+1 === 2 && console.log(true)
+                            //problem error of created_at null is solve
+                            // if(doc.data().created_at) {
+                            //     result.push(document)
+                            // }
+                            // if created_at is data inside run this code result.push(document)
+                            // 1+1 === 2 && console.log(true)
                 doc.data().created_at && result.push(document)
                   
             } )
                 messages.value = result
 
         })
-        return {messages}
+        return {messages,formattedMessages,msgBox}
     }
 
 }
@@ -67,9 +84,10 @@ export default {
     margin-right: 6px;
 
 }
-.messages {
+.message {
     max-height: 400px;
     overflow:auto;
 }
+
 
 </style>
